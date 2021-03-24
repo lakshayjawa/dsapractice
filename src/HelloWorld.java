ee800bd7911eb3b80a52fe36eeb6d48d6994ade7
lakshayjawa <38829884+lakshayjawa@users.noreply.github.com>
2021-03-24 16:50:23 +0530

    More Programs

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,5 @@
-import java.util.Map;
-import java.util.TreeMap;
-
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello World");
-    Map<Integer,Integer> d = new TreeMap<>();
-    //d.putIfAbsent()
   }
 }
